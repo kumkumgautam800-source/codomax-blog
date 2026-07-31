77297a008afa00f7927d9b37b93ada56b4aa6ce9
kumkum gautam <kumkumgautam800@gmail.com>
2026-07-31 12:48:08 +0530

    Day 6 Completed

--- index.js
+++ index.js
@@ -3,38 +3,49 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-// Middleware
 app.use(express.json());
+
+// JavaScript Array
+let blogs = [
+    {
+        id: 1,
+        title: "JavaScript Basics",
+        author: "Admin",
+        category: "Programming",
+        content: "Learn JavaScript step by step."
+    }
+];
 
 // Home Route
 app.get("/", (req, res) => {
     res.send("Welcome to Codomax Blog API 🚀");
 });
 
-// GET Route
+// GET All Blogs
 app.get("/blogs", (req, res) => {
-    res.json([
-        {
-            id: 1,
-            title: "JavaScript Basics",
-            author: "Kumkum"
-        }
-    ]);
+    res.json(blogs);
 });
 
-// POST Route
+// POST Blog
 app.post("/blogs", (req, res) => {
 
-    const blog = req.body;
+    const newBlog = {
+        id: blogs.length + 1,
+        title: req.body.title,
+        author: req.body.author,
+        category: req.body.category,
+        content: req.body.content
+    };
 
-    res.json({
+    blogs.push(newBlog);
+
+    res.status(201).json({
         message: "Blog Added Successfully",
-        data: blog
+        blog: newBlog
     });
 
 });
 
-// Server Start
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
